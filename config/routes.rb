408b2fba7962f91f40d996543bbfe91af624542c
Isaiah <duenasij3279@student.laccd.edu>
2024-07-15 14:39:27 +0000

    Who Knows

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'about_us/aboutus'
-  get 'home/home'
-  get 'login/login'
-  get 'contact_us/contactus'
-  get 'services/service'
+  root 'home#home'
+
+  get '/home', to: 'home#home'
+  get 'aboutus', to: 'about_us#aboutus'
+  get 'services', to: 'services#service'
+  get 'contactus', to: 'contact_us#contactus'
+  get 'login', to: 'login#login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
